257ded9963b0fc83d20adde260d0ac9c20a382e2
Rohit Kaushik <kaushikrohit325@gmail.com>
2018-05-05 14:14:47 +0530

    add compiled executable and build files

---
+++ build/CMakeFiles/resample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/resample.dir/resampling.cpp.o"
+  "resample.pdb"
+  "resample"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/resample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/rgbd_mapping.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "moc_MapBuilder.cpp"
+  "CMakeFiles/rgbd_mapping.dir/main.cpp.o"
+  "CMakeFiles/rgbd_mapping.dir/moc_MapBuilder.cpp.o"
+  "rgbd_mapping.pdb"
+  "rgbd_mapping"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rgbd_mapping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
